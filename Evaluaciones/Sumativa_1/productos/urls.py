fd06b90609e78f231507e23bfc062d64154f5812
yskaaren <karengutierrez0414@gmail.com>
2024-09-21 23:53:38 -0300

    Ultimos cambios

--- Evaluaciones/Sumativa_1/productos/urls.py
+++ Evaluaciones/Sumativa_1/productos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('productos', views.product)
+    path('productos', views.product),
+    path('registrar/', views.registrar_producto, name='registrar_producto'),
+    path('consultar_productos/', views.consultar_productos, name='consultar_productos'),
 ]
--- Evaluaciones/Sumativa_1/productos/views.py
+++ Evaluaciones/Sumativa_1/productos/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
+productos_memoria = []
 
 # Create your views here.
 
@@ -6,3 +9,25 @@ def product(request):
     print("Productos")
 
     return render(request, 'index.html')
+
+def registrar_producto(request):
+    if request.method == 'POST':
+        codigo = request.POST.get('codigo')
+        nombre = request.POST.get('nombre')
+        marca = request.POST.get('marca')
+        fecha_vencimiento = request.POST.get('fecha_vencimiento')
+
+        nuevo_producto = {
+            'codigo': codigo,
+            'nombre': nombre,
+            'marca': marca,
+            'fecha_vencimiento': fecha_vencimiento,
+        }
+        productos_memoria.append(nuevo_producto)
+
+        return render(request, 'resultado.html', {'producto': nuevo_producto})
+
+    return render(request, 'registro.html')
+
+def consultar_productos(request):
+    return render(request, 'consulta.html', {'productos': productos_memoria})
