75f482861f72ec241c7025274539b72e34f695f5
yskaaren <karengutierrez0414@gmail.com>
2024-10-14 22:07:32 -0300

    Creacion de modelos, superuser y edición de archivo README

--- Evaluaciones/Sumativa_1/productos/apps.py
+++ Evaluaciones/Sumativa_1/productos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GestionAppConfig(AppConfig):
+class ProductosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestion_app'
+    name = 'productos'
--- Evaluaciones/Sumativa_1/productos/models.py
+++ Evaluaciones/Sumativa_1/productos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    codigo = models.CharField(max_length=10, unique=True)
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.codigo}"
+
+class Caracteristica(models.Model):
+    producto = models.ForeignKey(Producto, related_name="caracteristicas", on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    valor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre}: {self.valor}"
